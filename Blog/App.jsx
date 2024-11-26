3f7b74743ed4a4a4ac91bfb894e484045ef4a6e6
Jaspreet Kaur <122146384+its-jaspreet@users.noreply.github.com>
2024-11-26 23:17:38 +0530

    Uploaded to Blog

---
+++ Blog/App.jsx
@@ -0,0 +1,49 @@
+// // import React, { useState } from 'react'
+// // import "./App.css"
+
+// import "../src/stylesheets/blogMain.css"
+
+// import BlogDate from './components/BlogDate'
+// import BlogTitle from './components/BlogTitle'
+// import BlogBrief from './components/BlogBrief'
+// import BlogTable from './components/BlogTable'
+// import BlogText from './components/BlogText'
+// import BlogSubtitle from './components/BlogSubtitle'
+// import BlogList from './components/BlogList'
+// import BlogSubsubtitle from './components/BlogSubsubtitle'
+
+// function App() {
+
+//     return (
+//         <div className="blog">
+//             <BlogDate date={28} month="October" year={2024}/>
+//             <BlogTitle title="How Yoga and Physiotherapy Can Help You Manage Bodily Changes During Pregnancy"></BlogTitle>
+
+//             <BlogText text="Yoga and Physiotherapy are forms of exercise that help us in maintaining physical and mental health of our body. During pregnancy following yoga asana and physiotherapy exercises plays an important role in helping the mother to cope with the stresses accompanying pregnancy and also helps in better development of the baby inside her womb. According to a study, pregnant women who performed yoga were found to be less likely to deliver a premature or low-weight baby."/>
+
+//             <BlogSubtitle subtitle="Benefits of doing Yoga and Physiotherapy during pregnancy"/>
+//             <BlogList type="ordered" items={[
+//                 <><strong>Increased flexibility and muscle strength</strong><br/>Yoga and Physiotherapy for pregnant women provide specific exercises that can help with stretching, flexibility and building up muscle strength. It’s recommended that strength building for pregnant women includes all the major muscle groups. Asans like Tadasana and Virbhadrasasna help to stretch the spine and thus impart flexibility.<br/><br/></>,
+//                 <><strong>Better mental health</strong><br/>Both Yoga and Physiotherapy use focused breathing and mindfulness, which have been shown to improve mental health by reducing stress, decreasing symptoms of anxiety and depression, improving mood and promoting relaxation. These exercises also increase oxygen supply to the developing foetus thus making a healthier baby. The yogic exercises and techniques help in calming the perturbed mind and impart a soothing effect. Relaxation methods which include meditation and visual imagery help in detaching yourself from the world for a while and helps in experiencing extreme bliss.<br/><br/></>,
+//                 <><strong>Reduced back pain</strong><br/>Yoga may also reduce lower back pain, depending on the type of pain you have. Some yoga poses stretch and strengthen the muscles and tissues associated with the lower back, hips, and hamstrings. A relaxed body aids in proper digestion and helps in developing the immune system.  If you have back pain, it’s best to check with your physiotherapist to get relief by physiotherapy for back pain or doctor and let them guide you for the same. Performing Marjariasana Or Cat and Camel exercise is very safe during pregnancy and helps in relieving back pain to major extent.<br/><br/></>, 
+//                 <><strong>Helps in developing stamina and strength</strong><br/>These exercises strengthen the core muscles of abdomen and pelvic region. This proves to be very beneficial to the pregnant women during the process of labor. Moreover the breathing exercises enhance their stamina which is an essential requirement for easy labor. Strengthening exercises for the knees , upper back and shoulders is a must as weakness in these areas can persist later on. Exercises with weights as less as half kg can be initiated. Light aerobics under supervision of a trained professional can also play an important role in building stamina and strength.<br/><br/></>,
+//                 <><strong>Connect with your baby growing inside</strong><br/>Most of the women often tend to neglect their personal care during pregnancy; yoga exercises can provide you that much needed time to look after yourself. In the hustle and bustle of life, yoga calms and relaxes your mind, and you can focus on the little things that are happening inside of you. Yoga keeps your strengthens the mother-baby bonding by cutting down your physical fatigue. It makes your pregnancy a lifetime experience-both enjoyable and memorable.<br/><br/></>,
+//                 <><strong>Increases chances of natural child birth</strong><br/>The likelihood of having normal childbirth is increased by practicing yoga during pregnancy. The muscles of the abdomen particularly the lower abdomen and pelvis are toned and strengthened by yogic exercises. Expectant women practicing yoga are less likely to have low birth weight children as compared to those not doing any yoga exercises. Yoga also helps to reduce labor anxiety and improves physical comfort. Wall supported squats and butterfly stretches help in preparing for labor.<br/><br/></>,
+//                 <><strong>Stronger pelvic floor</strong><br/>Physiotherapy classes especially designed for pregnancy often include pelvic floor exercises. Kegel’S exercises help to strengthen and tone the muscles supporting your pelvic floor, which can stop accidental leakage of urine during pregnancy or after your baby is born.<br/><br/></>,
+//                 <><strong>Keeps you active</strong><br/>Yoga can also help you break the lethargy and fatigue commonly seen during pregnancy. ‘Feel happy hormones’ are released in blood circulation that is responsible for generating a feeling of wellbeing- result is the development of felling of happiness and exuberance in expectant mothers. Practice yoga and stay away from pregnancy associated blues!<br/><br/></>,
+//                 <><strong>Aids in digestion</strong><br/>As the baby grows inside, your body’s digestive tract takes a hit and the organs fight for space. This makes your digestive system lose its regularity and causes indigestion, constipation, bloating and gas. Yoga exercises in the form of forward folds can help regularize the digestion to a great extent and enhance the digestive flow.<br/><br/></>,
+//                 <><strong>Reduces pain during child birth</strong><br/>The breathing exercises are very helpful in reducing pain experienced during pregnancy. Because yoga induces conscious breathing, while in labour, you will be able to regulate your heart rate and blood pressure. This will lead to an increased control over pain, and delivery will be easier.</>,
+//             ]}/>
+//         </div>
+//     );
+// }
+
+// export default App;
+
+
+import MentalHealth3 from "./articles/MentalHealthHowYogaAndPhysiotherapyCanHelpYouManageBodilyChangesDuringPregnancy";
+function App() {
+    return <MentalHealth3/>;
+}
+
+export default App;
